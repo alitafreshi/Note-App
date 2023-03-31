16566171fa9cb758e0335c15303daa8334a133ec
alitafreshi <alitafreshi123@yahoo.com>
2023-03-31 19:39:17 +0430

    Adding Modules To The Core

--- buildSrc/src/main/kotlin/Modules.kt
+++ buildSrc/src/main/kotlin/Modules.kt
@@ -1,15 +1,15 @@
 object Modules {
 
     const val app = ":app"
-    const val resources = ":resource"
-    const val components = ":components"
-    const val data = ":data"
+    const val resources = ":core:resource"
+    const val components = ":core:components"
+    const val data = ":core:data"
 
     //core
     const val constance = ":core:constance"
 
     //state manager
-    const val stateManager=":state-manager"
+    const val stateManager=":core:state-manager"
 
     //task
     const val taskList = ":task:task-list"
