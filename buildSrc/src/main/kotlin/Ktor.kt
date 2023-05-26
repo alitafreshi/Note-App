f9bce0014b4274314a684ec4af40d92593e2a647
alitafreshi <alitafreshi123@yahoo.com>
2023-05-26 15:32:40 +0330

    refactor

--- buildSrc/src/main/kotlin/Ktor.kt
+++ buildSrc/src/main/kotlin/Ktor.kt
@@ -1,11 +1,4 @@
 object Ktor {
-    /*private const val retrofit_version = "2.9.0"
-    private const val gson_version = "2.10.1"
-    const val retrofit = "com.squareup.retrofit2:retrofit:$retrofit_version"
-    const val gson = "com.google.code.gson:gson:$gson_version"
-    const val gson_convertor = "com.squareup.retrofit2:converter-gson:$retrofit_version"
-*/
-
     private const val ktor_client_version = "2.3.0"
 
     const val ktor_client_core = "io.ktor:ktor-client-core:$ktor_client_version"
